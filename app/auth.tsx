e47734964535198870f56b966b7326424d25018c
Atharv Dange <atharvdange618@gmail.com>
2025-07-05 23:03:21 +0530

    feat: Implement email authentication
    Adds a complete email-based authentication flow.
    
    - Introduces an AuthContext to manage user state and provide authentication functions (sign in, sign up, sign out) using Appwrite.
    - Creates a new AuthProvider to wrap the application and make the authentication context available to all components.
    - Implements the UI for the authentication screen in app/auth.tsx, allowing users to sign in or sign up with their email and password.
    - Includes error handling and user feedback for a smoother authentication experience.

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,9 +1,49 @@
+import { useAuth } from "@/lib/auth-context";
+import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
 const AuthScreen = () => {
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
+
+  const theme = useTheme();
+  const router = useRouter();
+
+  const { signIn, signUp } = useAuth();
+
+  const handleAuth = async () => {
+    if (!email || !password) {
+      setError("Please fill in all fields.");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Passwords must be at least 6 characters long.");
+      return;
+    }
+
+    setError(null);
+
+    if (isSignUp) {
+      const error = await signUp(email, password);
+      if (error) {
+        setError(error);
+        return;
+      }
+    } else {
+      const error = await signIn(email, password);
+      if (error) {
+        setError(error);
+        return;
+      }
+
+      router.replace("/");
+    }
+  };
 
   const handleSwitchMode = () => {
     setIsSignUp((prev) => !prev);
@@ -25,6 +65,7 @@ const AuthScreen = () => {
           autoCapitalize="none"
           mode="outlined"
           style={styles.input}
+          onChangeText={setEmail}
         />
 
         <TextInput
@@ -33,9 +74,12 @@ const AuthScreen = () => {
           keyboardType="email-address"
           mode="outlined"
           style={styles.input}
+          onChangeText={setPassword}
         />
 
-        <Button style={styles.button} mode="contained">
+        {error && <Text style={{ color: theme.colors.error }}> {error}</Text>}
+
+        <Button style={styles.button} mode="contained" onPress={handleAuth}>
           {isSignUp ? "Sign Up" : "Sign In"}
         </Button>
         <Button
